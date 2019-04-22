0897b4d5dd735ccacf120628e8ad63a6ac1fa440
sfanpro <st.sergeyf@gmail.com>
2019-04-22 23:12:02 +0300

    basic security

---
+++ spring-security/src/main/java/com/goodteam/springsecurity/demo/config/SecurityWebApplicationInicializer.java
@@ -0,0 +1,7 @@
+package com.goodteam.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInicializer extends AbstractSecurityWebApplicationInitializer {
+
+}
