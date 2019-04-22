86ed038f47276f0fc37bcb2e4cae9e1463ddab37
sfanpro <st.sergeyf@gmail.com>
2019-04-22 18:55:15 +0300

    commit

---
+++ spring-security/src/main/java/com/goodteam/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,25 @@
+package com.goodteam.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Component
+@EnableWebMvc
+@ComponentScan(basePackages = "com.goodteam.springsecurity.demo")
+public class DemoAppConfig {
+
+	// define a bean for ViewResolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+}
---
+++ spring-security/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Goodteam Company Home Page</title>
+	</head>
+	<body>
+		<h2>Goodteam Company Home Page</h2>
+		<hr>
+		You are Welcome!
+	</body>
+</html>
